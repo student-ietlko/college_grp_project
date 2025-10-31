16ac730203eb725b3994b3e74a8d45ebf5cf64d7
student-ietlko <nigambhavya6@gmail.com>
2025-10-31 21:29:56 +0530

    this is my second changes

--- my.cpp
+++ my.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"hello world from bhavya"<<endl;
+    cout<<"hello world from bhavya.this is from college iet"<<endl;
     return 0;
 }
